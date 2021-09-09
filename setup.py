1e89973dadd12bf4abc26f384e283f57089a7c4b
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-09 18:40:47 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Youtube-Video-Thumbnail/blob/main/LICENSE
+
+
+from setuptools import setup
