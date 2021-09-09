f86408ee753b7a9982108c5c92830fc8fbc940af
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-09 19:05:30 +0530

    Added setup

--- setup.py
+++ setup.py
@@ -4,5 +4,29 @@
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Youtube-Video-Thumbnail/blob/main/LICENSE
 
+import pathlib
+from setuptools import setup, find_packages
 
-from setuptools import setup
+
+file = pathlib.Path(__file__).parent
+
+README = (file / "README.md").read_text()
+
+setup = (
+    name="YouTube Video Thumbnail",
+    version="1.0.0",
+    author="FayasNoushad",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description="A youtube video link or id to video thumbnail python package.",
+    license="MIT",
+    url="https://github.com/FayasNoushad/Youtube-Video-Thumbnail",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
