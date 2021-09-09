cf83ec5fa324848f58297eff1e88b6975c0dfba4
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-09 18:27:56 +0530

    Added thumbnail defenition

--- ytthumb/__init__.py
+++ ytthumb/__init__.py
@@ -3,3 +3,15 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Youtube-Video-Thumbnail/blob/main/LICENSE
+
+
+def thumbnail(video):
+    if ("youtube.com" in video) and ("/" in video) and ("=" in video):
+        id = video.split("=")[-1]
+    elif ("youtu.be" in video) and ("/" in video):
+        id = video.split("/")[-1]
+    else:
+        id = video
+    thumbnail = "https://img.youtube.com/vi/" + id + "/sddefault.jpg"
+    return thumbnail
+
